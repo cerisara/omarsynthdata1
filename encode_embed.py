68fe872cfce23c05c8d6619c0ee98a2f39f1e3b9
Christophe Cerisara <cerisara@loria.fr>
2025-10-08 12:01:21 +0200

    encode on CPU

---
+++ encode_embed.py
@@ -0,0 +1,28 @@
+from transformers import pipeline
+
+model_name = "Qwen/Qwen3-Embedding-0.6B"
+
+# from transformers import AutoModel, AutoTokenizer
+# # Load tokenizer and model
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+# 
+# txt=["aa bb cc", "dd ee ff"]
+# inputs = tokenizer(txt, padding=True, truncation=True, return_tensors="pt")
+# 
+# with torch.no_grad():
+#     outputs = model(**inputs)
+# 
+# embeddings = outputs.last_hidden_state.mean(dim=1)  # mean pooling example
+
+embedder = pipeline("feature-extraction", model=model_name, trust_remote_code=True, device=-1)
+
+with open("ds6.txt","w") as g:
+    with open("DATASET6/final.csv","r") as f:
+        for l in f:
+            l=l[2:]
+            if l[0]=='"': l=l[1:-1]
+            embeddings = embedder(l)
+            g.write(str(embeddings)+'\n')
+            g.flush()
+
