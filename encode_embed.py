5a0bda4cc6a2819d860a7574a9feb19f1b8c49bd
Christophe Cerisara <cerisara@loria.fr>
2025-10-08 14:51:25 +0200

    ok

--- encode_embed.py
+++ encode_embed.py
@@ -18,7 +18,7 @@ model_name = "Qwen/Qwen3-Embedding-0.6B"
 embedder = pipeline("feature-extraction", model=model_name, trust_remote_code=True, device=-1)
 
 with open("ds6.txt","w") as g:
-    with open("DATASET6/final.csv","r") as f:
+    with open("shuffledds6.csv","r") as f:
         for l in f:
             l=l[2:]
             if l[0]=='"': l=l[1:-1]
---
+++ train.py
@@ -0,0 +1,66 @@
+
+import torch
+import torch.nn as nn
+import random
+
+es = []
+with open("ds6.txt","r") as f:
+    for li, l in enumerate(f):
+        s=l.replace('[','').strip().split(']')
+        vs=[]
+        for i in range(len(s)):
+            ss=s[i].strip().split(',')
+            v=[float(x) for x in ss if len(x)>0]
+            if len(v)>0: vs.append(v)
+        e = [0.]*len(vs[0])
+        for i in range(len(vs)):
+            for j in range(len(e)):
+                e[j] += vs[i][j]
+        for j in range(len(e)): e[j] /= float(len(vs))
+        es.append(torch.tensor(e))
+        if len(es)>300: break
+
+with open("shuffledds6.csv","r") as f:
+    for i,l in enumerate(f):
+        if i>=len(es): break
+        lab = int(l[0])
+        es[i] = (lab,es[i])
+
+# on garde toujours le meme test set
+testset = es[:200]
+es = es[200:]
+
+nsamp = len(es)
+dim = es[0][1].shape[0]
+nclass = 1+max([x[0] for x in es])
+print("data",nsamp,dim,nclass)
+
+mlp = nn.Sequential(
+    nn.Linear(dim, 256),
+    nn.ReLU(),              # or nn.GELU() for Transformer-style
+    nn.Linear(256, nclass)
+)
+
+labs = [torch.LongTensor([x]) for x in range(nclass)]
+lossf = nn.CrossEntropyLoss()
+opt = torch.optim.Adam(mlp.parameters(),lr=0.0001)
+for ep in range(1000):
+    random.shuffle(es)
+    opt.zero_grad()
+    for lab,x in es:
+        y=mlp(x)
+        loss = lossf(y.view(1,-1),labs[lab].view(1,))
+        print("LOSS",loss.item(),ep)
+        loss.backward()
+        opt.step()
+
+    with torch.no_grad():
+        nok,ntot=0,0
+        for lab,x in testset:
+            y=mlp(x)
+            cpred = torch.argmax(y)
+            if cpred.item()==lab: nok+=1            
+            ntot += 1
+            acc = float(nok)/float(ntot)
+            print("ACC",acc,ntot,cpred,lab)
+
