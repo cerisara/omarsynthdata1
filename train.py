d6d82bec30a856465c6a0a97f0267a45837f5910
xtof <cerisara@loria.fr>
2025-10-09 08:49:57 +0200

    eval with acc

--- train.py
+++ train.py
@@ -2,7 +2,27 @@
 import torch
 import torch.nn as nn
 import random
-
+ 
+def loadval():
+    with open("aclarcval.lab","r") as f: labs=[int(s) for s in f]
+    es=[]
+    with open("aclarcval.txt","r") as f:
+        for li, l in enumerate(f):
+            s=l.replace('[','').strip().split(']')
+            vs=[]
+            for i in range(len(s)):
+                ss=s[i].strip().split(',')
+                v=[float(x) for x in ss if len(x)>0]
+                if len(v)>0: vs.append(v)
+            e = [0.]*len(vs[0])
+            for i in range(len(vs)):
+                for j in range(len(e)):
+                    e[j] += vs[i][j]
+            for j in range(len(e)): e[j] /= float(len(vs))
+            es.append(torch.tensor(e))
+    return es,labs
+ 
+acles,acllabs = loadval()
 es = []
 with open("ds6.txt","r") as f:
     for li, l in enumerate(f):
@@ -18,7 +38,7 @@ with open("ds6.txt","r") as f:
                 e[j] += vs[i][j]
         for j in range(len(e)): e[j] /= float(len(vs))
         es.append(torch.tensor(e))
-        if len(es)>10200: break
+        if len(es)>520: break
 
 with open("shuffledds6.csv","r") as f:
     for i,l in enumerate(f):
@@ -38,15 +58,13 @@ print("data",nsamp,dim,nclass)
 mlp = nn.Sequential(
     nn.Linear(dim, 256),
     nn.ReLU(),              # or nn.GELU() for Transformer-style
-    nn.Linear(256, 256),
-    nn.ReLU(),              
     nn.Linear(256, nclass)
 )
 
 labs = [torch.LongTensor([x]) for x in range(nclass)]
 lossf = nn.CrossEntropyLoss()
 opt = torch.optim.Adam(mlp.parameters(),lr=0.00002)
-for ep in range(1000):
+for ep in range(100):
     random.shuffle(es)
     xi=0
     for lab,x in es:
@@ -76,4 +94,24 @@ for ep in range(1000):
         for ci in range(nclass):
             acc = float(nokc[ci])/float(ntotc[ci])
             print("ACL"+str(ci),acc,ntotc[ci])
-
+ 
+    with torch.no_grad():
+        nok,ntot=0,0
+        nokc = [0]*nclass
+        ntotc = [0]*nclass
+        for i in range(len(acles)):
+            y=mlp(acles[i])
+            cpred = torch.argmax(y)
+            lab = acllabs[i]
+            if cpred.item()==lab:
+                nokc[lab]+=1
+                nok+=1
+            ntot += 1
+            ntotc[lab]+=1
+            print("VALREC",cpred.item(),lab)
+        acc = float(nok)/float(ntot)
+        print("VALACC",acc,ntot)
+        for ci in range(nclass):
+            acc = float(nokc[ci])/float(ntotc[ci])
+            print("VALACL"+str(ci),acc,ntotc[ci])
+     
