05683403071df3139b98e066d690a8c5d51ff0c3
Christophe Cerisara <cerisara@loria.fr>
2025-10-10 07:55:07 +0200

    ok

--- prepacl.py
+++ prepacl.py
@@ -18,7 +18,28 @@ def preptrain():
             embeddings = embedder(l)
             g.write(str(embeddings)+'\n')
             g.flush()
+ 
+def preptest():
+    from datasets import load_dataset
+    from transformers import pipeline
 
+    model_name = "Qwen/Qwen3-Embedding-0.6B"
+    embedder = pipeline("feature-extraction", model=model_name, trust_remote_code=True, device=-1)
 
-preptrain()
+    ds = load_dataset("hrithikpiyush/acl-arc")
+    val = ds['test']
+    print("data",val)
+    with open("aclarctest.lab","w") as g:
+        for i in range(len(val)):
+            l=val['intent'][i]
+            g.write(str(l)+'\n')
+    with open("aclarctest.txt","w") as g:
+        for i in range(len(val)):
+            l=val['cleaned_cite_text'][i]
+            embeddings = embedder(l)
+            g.write(str(embeddings)+'\n')
+            g.flush()
+ 
+preptest()
+# preptrain()
 
--- train.py
+++ train.py
@@ -3,6 +3,27 @@ import torch
 import torch.nn as nn
 import random
  
+def loadtrain():
+    with open("aclarctrain.lab","r") as f: labs=[int(s) for s in f]
+    es=[]
+    with open("aclarctrain.txt","r") as f:
+        for li, l in enumerate(f):
+            s=l.replace('[','').strip().split(']')
+            vs=[]
+            for i in range(len(s)):
+                ss=s[i].strip().split(',')
+                v=[float(x) for x in ss if len(x)>0]
+                if len(v)>0: vs.append(v)
+            e = [0.]*len(vs[0])
+            for i in range(len(vs)):
+                for j in range(len(e)):
+                    e[j] += vs[i][j]
+            for j in range(len(e)): e[j] /= float(len(vs))
+            # es.append(e)
+            es.append(torch.tensor(e))
+    print("traindata",' '.join([str(sum([1 for l in labs if l==x])) for x in range(max(labs))]))
+    return es,labs
+ 
 def loadval():
     with open("aclarcval.lab","r") as f: labs=[int(s) for s in f]
     es=[]
@@ -22,37 +43,45 @@ def loadval():
             es.append(torch.tensor(e))
     return es,labs
  
+def loadsynth():
+    es = []
+    with open("ds6.txt","r") as f:
+        for li, l in enumerate(f):
+            s=l.replace('[','').strip().split(']')
+            vs=[]
+            for i in range(len(s)):
+                ss=s[i].strip().split(',')
+                v=[float(x) for x in ss if len(x)>0]
+                if len(v)>0: vs.append(v)
+            e = [0.]*len(vs[0])
+            for i in range(len(vs)):
+                for j in range(len(e)):
+                    e[j] += vs[i][j]
+            for j in range(len(e)): e[j] /= float(len(vs))
+            es.append(torch.tensor(e))
+            if len(es)>1200: break
+
+    labs=[]
+    with open("shuffledds6.csv","r") as f:
+        for i,l in enumerate(f):
+            if i>=len(es): break
+            lab = int(l[0])
+            labs.append(lab)
+    return es,labs
+
 acles,acllabs = loadval()
-es = []
-with open("ds6.txt","r") as f:
-    for li, l in enumerate(f):
-        s=l.replace('[','').strip().split(']')
-        vs=[]
-        for i in range(len(s)):
-            ss=s[i].strip().split(',')
-            v=[float(x) for x in ss if len(x)>0]
-            if len(v)>0: vs.append(v)
-        e = [0.]*len(vs[0])
-        for i in range(len(vs)):
-            for j in range(len(e)):
-                e[j] += vs[i][j]
-        for j in range(len(e)): e[j] /= float(len(vs))
-        es.append(torch.tensor(e))
-        if len(es)>1200: break
+# es,labs = loadsynth()
+es,labs = loadtrain()
+assert len(es)==len(labs)
+tridx = [i for i in range(len(es))]
 
-with open("shuffledds6.csv","r") as f:
-    for i,l in enumerate(f):
-        if i>=len(es): break
-        lab = int(l[0])
-        es[i] = (lab,es[i])
-
-# on garde toujours le meme test set
-testset = es[:200]
-es = es[200:]
+# on garde toujours la meme partie du synth data comme test set
+# testset = es[:200]
+# es = es[200:]
 
 nsamp = len(es)
-dim = es[0][1].shape[0]
-nclass = 1+max([x[0] for x in es])
+dim = es[0].shape[0]
+nclass = 1+max(labs])
 print("data",nsamp,dim,nclass)
 
 mlp = nn.Sequential(
@@ -65,35 +94,37 @@ labs = [torch.LongTensor([x]) for x in range(nclass)]
 lossf = nn.CrossEntropyLoss()
 opt = torch.optim.Adam(mlp.parameters(),lr=0.00002)
 for ep in range(100):
-    random.shuffle(es)
-    xi=0
-    for lab,x in es:
+    random.shuffle(tridx)
+    for xi in range(len(tridx)):
+        lab = torch.LongTensor(labs[tridx[xi]])
+        x   = es[tridx[xi]]
         opt.zero_grad()
         y=mlp(x)
-        loss = lossf(y.view(1,-1),labs[lab].view(1,))
+        loss = lossf(y.view(1,-1),lab.view(1,))
         print("LOSS",loss.item(),ep,xi)
         loss.backward()
         opt.step()
-        xi += 1
 
-    with torch.no_grad():
-        nok,ntot=0,0
-        nokc = [0]*nclass
-        ntotc = [0]*nclass
-        for lab,x in testset:
-            y=mlp(x)
-            cpred = torch.argmax(y)
-            if cpred.item()==lab:
-                nokc[lab]+=1
-                nok+=1            
-            ntot += 1
-            ntotc[lab]+=1
-            print("REC",ep,cpred.item(),lab)
-        acc = float(nok)/float(ntot)
-        print("ACC",acc,ntot)
-        for ci in range(nclass):
-            acc = float(nokc[ci])/float(ntotc[ci])
-            print("ACL"+str(ci),acc,ntotc[ci])
+    if False:
+        # only use that to test on part of synth data
+        with torch.no_grad():
+            nok,ntot=0,0
+            nokc = [0]*nclass
+            ntotc = [0]*nclass
+            for lab,x in testset:
+                y=mlp(x)
+                cpred = torch.argmax(y)
+                if cpred.item()==lab:
+                    nokc[lab]+=1
+                    nok+=1            
+                ntot += 1
+                ntotc[lab]+=1
+                print("REC",ep,cpred.item(),lab)
+            acc = float(nok)/float(ntot)
+            print("ACC",acc,ntot)
+            for ci in range(nclass):
+                acc = float(nokc[ci])/float(ntotc[ci])
+                print("ACL"+str(ci),acc,ntotc[ci])
  
     with torch.no_grad():
         nok,ntot=0,0
